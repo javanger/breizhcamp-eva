e1b42d7522aa13034a0c86dfd4ddc56749bfa184
Axel Buteau <buteauaxel@gmail.com>
2018-04-16 11:20:34 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export default class Main {
+    constructor() {
+        console.log('Typescript Webpack starter launched');
+    }
+}
+
+let start = new Main();
+alert('Conférence App démarré !');
