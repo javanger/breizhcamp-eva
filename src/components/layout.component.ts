0484dbd660682b90d246853af5ef692676ea16f9
Axel Buteau <buteauaxel@gmail.com>
2018-04-16 11:55:20 +0200

    ajout components

---
+++ src/components/layout.component.ts
@@ -0,0 +1,8 @@
+const template = `
+    <header>Ici Bientôt un logo</header>
+    <nav>Ici bientôt un menu</nav>
+    <section id="main-view">
+        Ici le contenu principal en attente de chargement
+    </section>
+    <footer>Ici bientôt un pied de page</footer>
+`
